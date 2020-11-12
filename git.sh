9c78acecea42d14dbda7f4d30b5265d42d7187bf
Donhyun72 <try365@gmail.com>
2020-11-12 14:09:31 +0000

    first commit

---
+++ git.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#git add $*
+git add -A
+
+#git config --global user.name "Donhyun72"
+#git config --global user.email "try365@hanmail.net"
+
+git commit -m "first commit"
+#git branch -M main
+git remote add origin https://github.com/Donhyun72/terraform-up-and-running-code-kr.git
+git push -u origin master
