a33a41543b906ee77f4b6f85d275ccbd8b61914e
Donhyun72 <try365@gmail.com>
2020-11-13 08:34:59 +0000

    Have modified some codes

--- code/terraform/03-terraform-state/file-layout-example/stage/services/webserver-cluster/main.tf
+++ code/terraform/03-terraform-state/file-layout-example/stage/services/webserver-cluster/main.tf
@@ -1,12 +1,14 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-southeast-1"
 }
 
 resource "aws_launch_configuration" "example" {
-  image_id        = "ami-40d28157"
+  image_id        = "ami-0c20b8b385217763f"
   instance_type   = "t2.micro"
-  security_groups = ["${aws_security_group.instance.id}"]
-  user_data       = "${data.template_file.user_data.rendered}"
+  # security_groups = ["${aws_security_group.instance.id}"]
+  security_groups = [aws_security_group.instance.id]
+  # user_data       = "${data.template_file.user_data.rendered}"
+  user_data       = data.template_file.user_data.rendered
 
   lifecycle {
     create_before_destroy = true
@@ -14,20 +16,27 @@ resource "aws_launch_configuration" "example" {
 }
 
 data "template_file" "user_data" {
-  template = "${file("user-data.sh")}"
+  # template = "${file("user-data.sh")}"
+  template = file("user-data.sh")
 
   vars {
-    server_port = "${var.server_port}"
-    db_address  = "${data.terraform_remote_state.db.address}"
-    db_port     = "${data.terraform_remote_state.db.port}"
+    # server_port = "${var.server_port}"
+    server_port = var.server_port
+    # db_address  = "${data.terraform_remote_state.db.address}"
+    db_address  = data.terraform_remote_state.db.address
+    # db_port     = "${data.terraform_remote_state.db.port}"
+    db_port     = data.terraform_remote_state.db.port
   }
 }
 
 resource "aws_autoscaling_group" "example" {
-  launch_configuration = "${aws_launch_configuration.example.id}"
-  availability_zones   = ["${data.aws_availability_zones.all.names}"]
+  # launch_configuration = "${aws_launch_configuration.example.id}"
+  launch_configuration = aws_launch_configuration.example.id
+  # availability_zones   = ["${data.aws_availability_zones.all.names}"]
+  availability_zones   = data.aws_availability_zones.all.names
 
-  load_balancers    = ["${aws_elb.example.name}"]
+  #@ load_balancers    = ["${aws_elb.example.name}"]
+  load_balancers    = [aws_elb.example.name]
   health_check_type = "ELB"
 
   min_size = 2
@@ -44,8 +53,10 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = "${var.server_port}"
-    to_port     = "${var.server_port}"
+    # from_port   = "${var.server_port}"
+    from_port   = var.server_port
+    # to_port     = "${var.server_port}"
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -59,13 +70,16 @@ data "aws_availability_zones" "all" {}
 
 resource "aws_elb" "example" {
   name               = "terraform-asg-example"
-  availability_zones = ["${data.aws_availability_zones.all.names}"]
-  security_groups    = ["${aws_security_group.elb.id}"]
+  # availability_zones = ["${data.aws_availability_zones.all.names}"]
+  availability_zones = data.aws_availability_zones.all.names
+  # security_groups    = ["${aws_security_group.elb.id}"]
+  security_groups    = [aws_security_group.elb.id]
 
   listener {
     lb_port           = 80
     lb_protocol       = "http"
-    instance_port     = "${var.server_port}"
+    # instance_port     = "${var.server_port}"
+    instance_port     = var.server_port
     instance_protocol = "http"
   }
 
@@ -100,8 +114,10 @@ data "terraform_remote_state" "db" {
   backend = "s3"
 
   config {
-    bucket = "${var.db_remote_state_bucket}"
-    key    = "${var.db_remote_state_key}"
-    region = "us-east-1"
+    # bucket = "${var.db_remote_state_bucket}"
+    bucket = var.db_remote_state_bucket
+    # key    = "${var.db_remote_state_key}"
+    key    = var.db_remote_state_key
+    region = "ap-southeast-1"
   }
 }
